89961aea4fda4673968b5c275cad8c264f58b82d
goutham-damodhar <goutham.damodhar@oghealthcare.com>
2025-08-19 16:19:48 +0530

    Update app.js url update

--- frontend/app.js
+++ frontend/app.js
@@ -1300,8 +1300,8 @@ const CLEAR_KEY = 'XR_CLEAR_ON_NEXT_CONNECT'; // '1' => wipe on next connect
 // ---------------- CONFIG ----------------
 console.log('[CONFIG] Loading configuration');
 // Update to your server URL as needed:
-const SERVER_URL = 'https://6dadc7bb887f.ngrok-free.app';  
-// const SERVER_URL = 'https://xr-messaging-geexbheshbghhab7.centralindia-01.azurewebsites.net';
+// const SERVER_URL = 'https://6dadc7bb887f.ngrok-free.app';  
+const SERVER_URL = 'https://xr-messaging-geexbheshbghhab7.centralindia-01.azurewebsites.net';
 
 /* ------------- XR_ID / NAME init (updated) ------------- */
 // XR ID is editable from the front-end before connecting
